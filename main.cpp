6766e58276fa17be3d03e476ee84064c5a554c6a
justt1n <tinnt0709@gmail.com>
2025-12-21 13:28:18 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
-//
-// Created by t1nng on 12/21/2025.
-//
+// main.cpp
+#include <iostream>
+#include "BacktrackingSolver.h"
 
-#include "main.h"
+using namespace std;
+
+void printBoard(const Board& board) {
+    for (const auto& row : board) {
+        for (int cell : row) {
+            cout << (cell ? "Q " : ". ");
+        }
+        cout << endl;
+    }
+    cout << "----------------\n";
+}
+
+int main() {
+    int n = 8; // Ví dụ n = 4
+    BacktrackingSolver solver;
+    vector<Board> results = solver.solve(n);
+
+    cout << "Tim thay " << results.size() << " loi giai cho ban co " << n << "x" << n << ":\n";
+    for (const auto& board : results) {
+        printBoard(board);
+    }
+
+    return 0;
+}
